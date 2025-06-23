47a90f408c129b4e5b84ab7488e8464a78a23350
NorichikaSugii <wawa60379@gmail.com>
2025-06-24 00:03:17 +0900

    Initial commit with Count_SwiftUI project

---
+++ Count_SwiftUI/Count_SwiftUI/Count_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Count_SwiftUIApp.swift
+//  Count_SwiftUI
+//
+//  Created by 杉井位次 on 2025/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Count_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
