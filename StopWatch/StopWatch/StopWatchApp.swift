9b8303f17be1c875124480bc254c319a6ca63b08
NorichikaSugii <wawa60379@gmail.com>
2025-06-25 14:59:49 +0900

    add StopWatch

---
+++ StopWatch/StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by 杉井位次 on 2025/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
