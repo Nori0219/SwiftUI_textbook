c29ebf88c9bdb21be6f684829283eff0ca60fc11
NorichikaSugii <wawa60379@gmail.com>
2025-06-28 02:52:23 +0900

    add List

---
+++ List/List/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by 杉井位次 on 2025/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationStack{
+            List {
+                ForEach(Animal.allCases, id: \.self) { animal in
+                    NavigationLink(destination: DetailView(animal: animal)) {
+                        HStack {
+                            Image(animal.iconName)
+                            Text(animal.name)
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle("動物")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ List/List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by 杉井位次 on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
